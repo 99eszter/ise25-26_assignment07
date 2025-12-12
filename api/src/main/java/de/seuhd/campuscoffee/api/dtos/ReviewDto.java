e7ad371545a6b8212d4175d2c1929d35b207839c
Eszter Richter <eszter.richter@stud.uni-heidelberg.de>
2025-12-12 10:56:35 +0100

    import angepasst

--- api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
@@ -1,8 +1,7 @@
 package de.seuhd.campuscoffee.api.dtos;
 
 import lombok.Builder;
-import org.jspecify.annotations.Nullable;
-
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
