c934523d1e9c77e7ecaf3d0077e1a532502ddffe
Eszter Richter <eszter.richter@stud.uni-heidelberg.de>
2025-12-12 10:18:33 +0100

    ReviewDto TODOs implementiert

--- api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
@@ -3,13 +3,23 @@ package de.seuhd.campuscoffee.api.dtos;
 import lombok.Builder;
 import org.jspecify.annotations.Nullable;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
 /**
  * DTO record for POS metadata.
  */
 @Builder(toBuilder = true)
-public record ReviewDto (
-    @Nullable Long id
-    // TODO: Implement ReviewDto
+public record ReviewDto(
+        @Nullable Long id
+        @Nullable Long id,
+        @Nullable Instant createdAt,
+        @Nullable Instant updatedAt,
+        @NotNull Long posId,
+        @NotNull Long authorId,
+        @NotBlank String review,
+        @Nullable Boolean approved
 ) implements Dto<Long> {
     @Override
     public @Nullable Long getId() {
